dc6471485a18a6a77a81070caea017c148b543fb
sungwoong.park <ritchiepark6@gmail.com>
2022-11-28 10:39:12 +0900

    Initial of HW4

---
+++ src/hw4_update.sql
@@ -0,0 +1,14 @@
+
+use courseman
+-- Harry Chung, pw=abc123
+
+UPDATE students SET password="e38804c45d255359322d3a3c9d58522fe0528e87784b6724fca79dd67b891dd" where id=1;
+
+-- Matt Lee, pw=def456
+
+UPDATE students SET password="0124f7d9cfb7da14c498781ff49752edf89cb08574cb19785aaa2a7b4722f5ed" where id=2;
+
+-- SUE Park, pw=rotfl1
+
+UPDATE students SET password="13a0461de73c38bf3aa73ccd4f207170cf1dece6300c9fc0248a9e0d70cb995a" where id=3;
+
